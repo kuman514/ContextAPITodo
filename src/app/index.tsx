feb240bbe1558a3a9078593ec9c512946cd3d908
kuman514 <hadjadj0@gmail.com>
2024-10-08 09:22:57 +0900

    chore: apply context to app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,9 @@
+import { TodoContextProvider } from '^/shared/context';
+
 export function App() {
-  return <main>Get ready for the next battle!</main>;
+  return (
+    <TodoContextProvider>
+      <main>Get ready for the next battle!</main>
+    </TodoContextProvider>
+  );
 }
