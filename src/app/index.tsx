a01d7910855e84cd7500d42354cc44f053a3a30c
kuman514 <hadjadj0@gmail.com>
2024-10-09 17:30:32 +0900

    draft: apply create-todo & todo-list to main app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,21 @@
+import { CreateTodo } from '^/features/create-todo';
+import { TodoList } from '^/features/todo-list';
 import { TodoContextProvider } from '^/shared/context';
 
 export function App() {
   return (
     <TodoContextProvider>
-      <main>Get ready for the next battle!</main>
+      <main
+        style={{
+          width: '100vw',
+          height: '100dvh',
+          display: 'grid',
+          gridTemplateColumns: '300px 1fr',
+        }}
+      >
+        <TodoList />
+        <CreateTodo />
+      </main>
     </TodoContextProvider>
   );
 }
