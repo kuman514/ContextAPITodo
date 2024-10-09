976a387b22b1db8b1b67751589fca6712e367e72
kuman514 <hadjadj0@gmail.com>
2024-10-09 09:32:34 +0900

    draft: add custom style & onclick to list-item

--- src/shared/list-item/index.tsx
+++ src/shared/list-item/index.tsx
@@ -1,11 +1,17 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 
 import './style.css';
 
 interface Props {
   children: ReactNode;
+  customStyle?: CSSProperties;
+  onClick: () => void;
 }
 
-export function ListItem({ children }: Props) {
-  return <li className="list-item">{children}</li>;
+export function ListItem({ children, customStyle, onClick }: Props) {
+  return (
+    <li className="list-item" style={customStyle} onClick={onClick}>
+      {children}
+    </li>
+  );
 }
