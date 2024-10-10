c1c74e9f09e62329789e49d51a22582051f1f23d
kuman514 <hadjadj0@gmail.com>
2024-10-10 12:17:23 +0900

    draft: add content section

---
+++ src/widgets/content-section/index.tsx
@@ -0,0 +1,20 @@
+import { CreateTodo } from '^/features/create-todo';
+import { ModifyTodo } from '^/features/modify-todo';
+import { TodoDetail } from '^/features/todo-detail';
+import { useTodoContext } from '^/shared/context';
+import { AppMode } from '^/shared/types';
+
+export function ContentSection() {
+  const { appMode } = useTodoContext();
+
+  switch (appMode) {
+    case AppMode.CREATE:
+      return <CreateTodo />;
+    case AppMode.MODIFY:
+      return <ModifyTodo />;
+    case AppMode.DETAIL:
+      return <TodoDetail />;
+    default:
+      return null;
+  }
+}
