5d37df19353973a7c02eef5f6c2283edc752f284
kuman514 <hadjadj0@gmail.com>
2024-10-11 22:38:31 +0900

    improvement: add storing todo data to local storage

--- src/shared/context/index.tsx
+++ src/shared/context/index.tsx
@@ -1,6 +1,55 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 import { AppMode, TodoItem, TodoItemContext } from '^/shared/types';
+
+const DEFAULT_TODOS: TodoItem[] = [
+  {
+    id: 0,
+    title: 'Context API Practice',
+    content: 'Yasuo is science.',
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+  },
+  {
+    id: 1,
+    title: 'by kuman514',
+    content: 'If you win, you are Maverick. If you lose, you are Yasuo.',
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+  },
+  {
+    id: 2,
+    title: 'Koishi is an adorable Aggie',
+    content: 'Koishi Komeiji is an adorable baby. Aggie Koishi Aggie Hoshino.',
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+  },
+];
+
+function loadFromStorage() {
+  const rawData = localStorage.getItem('context-api-practice-todo-items');
+
+  if (!rawData) {
+    return DEFAULT_TODOS;
+  }
+
+  const restoredData = JSON.parse(rawData) as TodoItem[];
+  return restoredData.map((todo) => ({
+    ...todo,
+    createdAt: new Date(todo.createdAt),
+    modifiedAt: new Date(todo.modifiedAt),
+  }));
+}
+
+function saveToStorage(data: TodoItem[]) {
+  localStorage.setItem('context-api-practice-todo-items', JSON.stringify(data));
+}
 
 const TodoContext = createContext<TodoItemContext>({
   data: [],
@@ -24,7 +73,7 @@ interface TodoContextProviderProps {
 
 export function TodoContextProvider({ children }: TodoContextProviderProps) {
   const [selectedTodoId, setSelectedTodoId] = useState<TodoItem['id']>(-1);
-  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [todos, setTodos] = useState<TodoItem[]>(loadFromStorage());
   const [appMode, setAppMode] = useState<AppMode>(AppMode.DETAIL);
 
   function createTodoItem(newTodoItem: TodoItem) {
@@ -48,6 +97,10 @@ export function TodoContextProvider({ children }: TodoContextProviderProps) {
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
   }
+
+  useEffect(() => {
+    saveToStorage(todos);
+  }, [todos]);
 
   return (
     <TodoContext.Provider
